e5f483cb1059d8a4a71c183a689201939205e6ad
vinayakkalra <vinayak.kalra@quadbtech.com>
2020-06-14 21:15:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# template for website structure
+
+This template has bootstrap, fontawesome, owl carousal and jquery pre downloaded and added. Please use the same for building a site.
---
+++ php/link.php
@@ -0,0 +1,12 @@
+<?php
+
+// $link = mysqli_connect("localhost", "finstreet", "#Rajabeta420","finstreet");
+$link = mysqli_connect("localhost", "root", "", "finstreet_finchords");
+
+if (mysqli_connect_error()){
+    die("<script>console.log('There is a problem with mysql connection')</script>");
+}
+
+
+?>
+   
